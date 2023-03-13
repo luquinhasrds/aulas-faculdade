d9ff9f61968333baf993679f72cbc82d2ed1d964
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-13 20:14:47 -0300

    Update atividade 6.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 6.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 6.py
@@ -2,4 +2,4 @@ print("--calcule a potencia--")
 base = int(input("digite a base:"))
 expoente = int(input("digite o expoente:"))
 potencia = base**expoente
-print(base, "elevado a", expoente, "é igual a", potencia)
+print(f"{base}, elevado a {expoente} é igual a {potencia})
