08dc78e3a79ee3a96e5328949f6a6204c2bcfc19
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-13 20:15:40 -0300

    Update atividade 8.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 8.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 8.py
@@ -2,4 +2,4 @@ print("--calcular area--")
 base = float(input("digite o tamanho da area do retangulo em metros:"))
 altura = float(input("digite a altura do retangulo em metros:"))
 area = base*altura
-print("a area do retangulo é de", area, "metros cúbicos")
+print(f"a area do retangulo é de {area} metros cúbicos")
