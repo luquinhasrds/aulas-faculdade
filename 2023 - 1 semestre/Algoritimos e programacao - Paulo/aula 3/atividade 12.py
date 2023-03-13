9fd71e74a03fe55dae623b40e5288d709e8793f8
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-13 20:17:49 -0300

    Update atividade 12.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 12.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 12.py
@@ -2,4 +2,4 @@ print("--calcular distancia percorrida--")
 velocidade = float(input("digite a velociade do veiculo (em KM/H):"))
 tempo = float(input("digite o tempo de percurso (em horas):"))
 distancia = velocidade*tempo
-print("a distancia percorrida pelo veiculo foi de", distancia, "Km")
+print(f"a distancia percorrida pelo veiculo foi de {distancia} Km")
