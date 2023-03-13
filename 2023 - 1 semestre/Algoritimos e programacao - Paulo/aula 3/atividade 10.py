401b19f715bb35ba1bb9e44a9d5d9b056bc12271
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-13 20:17:03 -0300

    Update atividade 10.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 10.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 10.py
@@ -3,4 +3,4 @@ comprimento = float(input("digite em metros o comprimento da caixa:"))
 largura = float(input("digite em metros a largura da caixa:"))
 altura = float(input("digite em metros a altura da caixa:"))
 volume = comprimento*largura*altura
-print("o volume da caixa é de", volume, "metros cúbicos")
+print(f"o volume da caixa é de {volume} metros cúbicos")
