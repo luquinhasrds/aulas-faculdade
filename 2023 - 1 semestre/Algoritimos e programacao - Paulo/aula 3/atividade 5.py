ac92d026b68f4c3db2f915629573b8c22184a8de
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-13 20:11:46 -0300

    Update atividade 5.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 5.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 3/atividade 5.py
@@ -2,4 +2,4 @@ print("--resto da divisão--")
 numero1 = int(input("digite um numero:"))
 numero2 = int(input("digite outro numero:"))
 resto = numero1 % numero2
-print("o resto da divisão de", numero1, "por", numero2, "é", resto)
+print(f"o resto da divisão de {numero1} por {numero2}, é resto")
