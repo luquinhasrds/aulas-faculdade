05daee3df9608a9bb06e2976b4268788e6269b50
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-20 20:29:50 -0300

    Update at8.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 4/at8.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 4/at8.py
@@ -1,3 +1,9 @@
+#forma 1
 lado = 6
 area = round((((6**2)*1.7)/4), 2)
+print(f"a area do triangulo é {area}")
+
+#forma 2
+lado = 6
+area = ((6**2)*(3*(1/2)))/4
 print(f"a area do triangulo é {area}")
