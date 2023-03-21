cf354b02482ba21e6f7ba16d953350e28f86cb7c
luquinhasrds <lucas-rsantos@outlook.com>
2023-03-21 15:55:48 -0300

    portfolio com css

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 4/at14.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 4/at14.py
@@ -1,7 +1,4 @@
 economia = 12
 distancia = 360
-<<<<<<< HEAD
-=======
-litro = economia/distancia
-print (f"ele gastara {litro} litros de gasolina")
->>>>>>> 7f2ac3c79f0b264c7ffc77b0eaa1aa2049435cc9
+litro = distancia/economia
+print (f"Ele gastara {litro} litros de gasolina")
