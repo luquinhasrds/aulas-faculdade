05c6bf3028136d13bc8649e9326758bab50833c7
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-27 19:33:21 -0300

    Update at5.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at5.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at5.py
@@ -2,6 +2,7 @@ v0  = float(input("digite a velocidade inicial do objeto em m/s:"))
 a = float(input("digite a aceleracao do objeto em m/s:"))
 t = float(input("digite o tempo percorrido em segundos:"))
 
+# v0=velocidade inicial  -  a=aceleração  -  t=tempo percorrido
 # formula: s =vo*t+a*t**2/2
 
 distancia = v0*t+(a*(t**2))/2 
