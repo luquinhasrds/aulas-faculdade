181ffe0de913083cb96ef8a8d140de5fc9004447
Lucas Rosa Dos Santos <Lucas-rsantos@rede.ulbra.br>
2023-03-27 21:48:53 -0300

    at3 paulo att

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at3.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at3.py
@@ -1,7 +1,9 @@
 print("---digite um numero para ver se ele é impar ou par---")
 while True:
 	numero = int(input("Digite um numero:"))
-	if numero % 2 == 0:
-		print("O numero é par.")
-	else:
+	if numero % 2 != 0:
 		print("O numero é impar.")
+	elif numero == 0:
+		print("o numero é zero ")
+	else:
+		print("O numero é par.")
