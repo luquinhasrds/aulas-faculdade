6faa61acdb1393916531698a97b8320d90f5a9b6
luquinhasrds <lucas-rsantos@outlook.com>
2023-03-28 18:42:28 -0300

    atividades paulo

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at tabuada 2.0.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at tabuada 2.0.py
@@ -1,5 +1,8 @@
 while True:
     num = int(input("DIGITE O NUMERO QUE QUER VER A TABUADA: "))
+    if num < 0:
+        print('FINALIZANDO O CODIGO')
+        break
     print('-'*30)
     for tabuada in range(1, 11):
         print(f'{num} x {tabuada} = {num*tabuada}')
--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at10.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at10.py
@@ -1,4 +1,2 @@
 notas = [float(input("Digite a nota:")) for _ in range(5)]
-soma_notas = sum(notas)
-quantidade = len(notas)
-print(f"a media das notas é {soma_notas/quantidade}")
+print(f"a media das notas é {sum(notas)/len(notas)}")
