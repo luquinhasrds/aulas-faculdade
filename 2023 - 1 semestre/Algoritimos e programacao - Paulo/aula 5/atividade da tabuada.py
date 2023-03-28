5703ab4be4e96f10471d0c35d6cbac8dfe1c40e7
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-28 12:31:06 -0300

    Atualizar o atividade da tabuada.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/atividade da tabuada.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/atividade da tabuada.py
@@ -1,5 +1,5 @@
 while True:
-    num = int(input("DIGITE O NUMERO QUE QUER VER A TABUADA"))
+    num = int(input("DIGITE O NUMERO QUE QUER VER A TABUADA: "))
     print('-'*30)
     for tabuada in range(1, 11):
         print(f'{num} x {tabuada} = {num*tabuada}')
