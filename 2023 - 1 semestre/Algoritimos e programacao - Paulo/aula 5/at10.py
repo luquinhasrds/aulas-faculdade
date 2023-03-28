52bdeccf99dca8b3953c7d5595067d2b4412b52a
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-28 11:30:41 -0300

    Update at10.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at10.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/at10.py
@@ -1,4 +1,4 @@
-notas = [float(input("Digite a nota:")) for c in range(5)]
+notas = [float(input("Digite a nota:")) for _ in range(5)]
 soma_notas = sum(notas)
 quantidade = len(notas)
 print(f"a media das notas é {soma_notas/quantidade}")
