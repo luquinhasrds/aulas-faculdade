d729a384bac53da0ce4a8ed1f3dfc3ca471304f6
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-03-28 12:12:04 -0300

    Create atividade da tabuada.py

---
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 5/atividade da tabuada.py
@@ -0,0 +1,6 @@
+while True:
+    num = int(input("DIGITE O NUMERO QUE QUER VER A TABUADA"))
+    print('-'*30)
+    for tabuada in range(1, 11):
+        print(f'{num} x {tabuada} = {num*tabuada}')
+    print('-'*30)
