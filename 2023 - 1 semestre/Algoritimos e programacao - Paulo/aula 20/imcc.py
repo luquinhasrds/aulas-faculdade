bcfb18585bfcdb2a0fc60860e67a2458d672a046
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-06-20 10:21:51 -0300

    Update imcc.py

--- 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 20/imcc.py
+++ 2023 - 1 semestre/Algoritimos e programacao - Paulo/aula 20/imcc.py
@@ -1,22 +1,30 @@
-import datetime as dt #importacao do modulo
-
-#modulo para a data
-data = dt.datetime.now()
-data_formatado = data.strftime('%m/%d/%y, %H:%M:%S')
+import datetime as dt
 
 def imc(peso, altura):
-    return round(peso/(altura**2), 2)
+    calc = round(peso/(altura**2), 2)
+    return calc
+def classificacao(calc):
+    if calc < 18.5:
+        return 'seu IMC é classificado como MU  ITO MAGRO'
+    elif calc < 24.9:
+        return 'Seu IMC é classificado como NORMAL '
+    elif calc < 29.9:
+        return 'Seu IMC é classificado como SOBREPESO'
+    elif calc < 34.9:
+        return 'Seu IMC é classificado como OBESIDADE I'
+    elif calc < 39.9:
+        return 'Seu IMC é classificado como OBESIDADE II'
+    else:
+        return 'Seu IMC é classificado como OBESIDADE III'
+nome = input('Digite o seu nome: ')
+peso = float(input('Digite o seu peso: '))
+altura = float(input('Digite a sua altura: '))
+data = dt.datetime.now()
+data_formatada = data.strftime('%d/%m/%y, %H:%M:%S')
+imc_avaliado = imc(peso, altura)
+imc_classificado = classificacao(imc_avaliado)
 
-#dados
-print('Digite os seus dados: ')
-idade = int(input('Sua idade: '))
-altura = float(input('Sua altura (separado com "."): '))
-peso = float(input('Seu peso: '))
-
-#dados do usuario
-print('-'*60)
-print('Dados do usuario:')
-print('-'*60)
-print(f'Data da consulta: {data_formatado}')
-print(f'---Idade do usuario: {idade} anos---')
-print(f'---IMC do usuario: {imc(peso, altura)}---')
+print(f'ola {nome}!')
+print(f'Seu imc é {imc_avaliado}')
+print(imc_classificado)
+print(f'essa avaliacao foi realizada em: {data_formatada}')
