add0bb55ad976bdd3af6be4f65e11cb22b0de66f
lucas rosa dos santos <lucas-rsantos@outlook.com>
2023-06-26 18:49:21 -0300

    ATT JULIANO E OUTRO ACHO

---
+++ 2023 - 1 semestre/Desenvolvimento Web - Juliano/aula 22 - tde 22/script.js
@@ -0,0 +1,40 @@
+
+//botao adicionar
+function adicionar() {
+    const tarefa = document.getElementById('tarefa').value
+    const li = document.createElement('li')
+    li.textContent = tarefa
+
+    //coloca as li para dentro da ol 'pendentes'
+    const pendentes = document.getElementById('pendentes')
+    pendentes.appendChild(li)
+
+//botao remover 
+    const remover = document.createElement('button')
+    remover.textContent = 'Remover'
+    remover.type = 'button'
+    remover.style.marginLeft = '20px'
+    remover.addEventListener('click', function () { li.remove() })
+    li.appendChild(remover)
+
+// Botão Concluir
+    const concluir = document.createElement('button');
+    concluir.textContent = 'Concluir';
+    concluir.type = 'button';
+    concluir.style.marginLeft = '20px';
+    concluir.addEventListener('click', function () {
+        const tarefa = this.parentNode; // Obtém o elemento pai (li) do botão Concluir.          confesso que esse nao entendi muito bem, pesquisei como usar na net
+        const concluidas = document.getElementById('concluidas');
+        concluidas.appendChild(tarefa);
+    });
+    li.appendChild(concluir);
+
+    // verifica se esta em branco o imput
+    document.getElementById('tarefa').value = ''
+    document.getElementById('tarefa').focus()
+}
+
+function mudacor(){
+    const body = document.querySelector('body')
+    body.classList.toggle('fundo') //Adiciona e remove
+}
