018d86f8ce7fb46e6833f143b44b16865532442e
lucas rosa dos santos <lucas-rsantos@outlook.com>
2023-10-31 20:25:06 -0300

    arrumando o trabalho da ap2

--- 2023 - 2 semestre/Lab. Programação/ap2 - parte-2/Gerenc.Capeonato/Program.cs
+++ 2023 - 2 semestre/Lab. Programação/ap2 - parte-2/Gerenc.Capeonato/Program.cs
@@ -2,62 +2,42 @@
 Jogador j1 = new Jogador();
 j1.nome = "Jorge";
 j1.nickName = "Jorginhoda12";
-j1.Jogar();
-j1.ExibirPontos();
 
 Jogador j2 = new Jogador();
 j2.nome = "Pedro";
 j2.nickName = "matadordeonça";
-j2.Jogar();
-j2.ExibirPontos();
 
 Jogador j3 = new Jogador();
 j3.nome = "Mario";
 j3.nickName = "OdoArmario";
-j3.Jogar();
-j3.ExibirPontos();
 
 Jogador j4 = new Jogador();
 j4.nome = "Diojines";
 j4.nickName = "Senijoid";
-j4.Jogar();
-j4.ExibirPontos();
 
 Jogador j5 = new Jogador();
 j5.nome = "Jean";
 j5.nickName = "Destruidor";
-j5.Jogar();
-j5.ExibirPontos();
 
 Jogador j6 = new Jogador();
 j6.nome = "Klaiton";
 j6.nickName = "AirtonSena";
-j6.Jogar();
-j6.ExibirPontos();
 
 Jogador j7 = new Jogador();
 j7.nome = "Leonardo";
 j7.nickName = "CabecaDeCaixaD'agua";
-j7.Jogar();
-j7.ExibirPontos();
 
 Jogador j8 = new Jogador();
 j8.nome = "Joel";
 j8.nickName = "OGerente";
-j8.Jogar();
-j8.ExibirPontos();
 
 Jogador j9 = new Jogador();
 j9.nome = "Andreia";
 j9.nickName = "BocaDeCaçapa";
-j9.Jogar();
-j9.ExibirPontos();
 
 Jogador j10 = new Jogador();
 j10.nome = "Julia";
 j10.nickName = "CabeçaDeRolon";
-j10.Jogar();
-j10.ExibirPontos();
 
 //=========================================//
 
@@ -87,12 +67,12 @@ equipe2.AdicionarJogadores(j10);
 
 //===========================================//
 
-// Crie o campeonato
+// Cria o campeonato
 Campeonato meuCampeonato = new Campeonato();
 meuCampeonato.NomeCampeonato = "Ribeirão Preto";
 meuCampeonato.AdicionarEquipe(equipe1);
 meuCampeonato.AdicionarEquipe(equipe2);
 
-// Inicie a partida e mostre a classificação
+// Inicia a partida e mostra a classificação
 meuCampeonato.IniciarPartida(equipe1, equipe2);
 meuCampeonato.Classificacao();
