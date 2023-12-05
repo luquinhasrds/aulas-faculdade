aaf894daeb23fa3d854e2da7a9fcd28a68f422a8
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-12-05 12:43:15 -0300

    Update Livro.cs

--- 2023 - 2 semestre/Lab. Programação/AS parte2/AS_Parte2/Livro.cs
+++ 2023 - 2 semestre/Lab. Programação/AS parte2/AS_Parte2/Livro.cs
@@ -1,10 +1,10 @@
-public class Livro : ItemBiblioteca{
-    public string? Autor { get; set; }
-    public Livro(int id, string titulo, string autor, string descricaoInterna) : base(id, titulo){
+public class Livro : ItemBiblioteca{  //CLASSE COM HERANÇA 
+    public string? Autor { get; set; }  //AUTOR
+    public Livro(int id, string titulo, string autor, string descricaoInterna) : base(id, titulo){  //CONSTRUTOR TRAZENDO A HERANCA
         this.Autor = autor;
         DescricaoInterna = descricaoInterna;
     }
-    public void ExibirDescricaoInterna(){
+    public void ExibirDescricaoInterna(){  //METODO PARA EXIBIR DESCRIÇÃO DO LIVRO
         Console.WriteLine(DescricaoInterna);
     }
-}
+}  //FECHAMENTO DA CLASSE
